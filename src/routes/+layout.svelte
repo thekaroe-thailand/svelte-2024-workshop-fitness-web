0fa1b8b8c34008db5e2a6c9f5580540f2932cdb8
thekaroe-thailand <thekaroe@gmail.com>
2024-08-05 19:31:53 +0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Navbar from "../components/Navbar.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+</script>
+
+<Navbar />
+
+<Sidebar />
+
+<div class="content-wrapper">
+  <section class="content">
+    <div class="container-fluid">
+      <slot />
+    </div>
+  </section>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+  import { goto } from "$app/navigation";
+  import axios from "axios";
+  import Swal from "sweetalert2";
+
+  let username = "";
+  let password = "";
+
+  async function signin() {
+    try {
+      if (username == "" || password == "") {
+        Swal.fire({
+          title: "ตรวจสอบข้อมูล",
+          text: "โปรดกรอกข้อมูลด้วย",
+          icon: "warning",
+        });
+      } else {
+        const payload = {
+          username: username,
+          password: password,
+        };
+
+        const res = await axios.post(
+          "http://localhost:3000/api/user/singin",
+          payload
+        );
+
+        if (res.data.token !== undefined) {
+          localStorage.setItem("fitness_token", res.data.token);
+          goto("/home");
+        }
+      }
+    } catch (e) {
+      Swal.fire({
+        title: "error",
+        text: e.message,
+        icon: "error",
+      });
+    }
+  }
+</script>
+
+<div class="card mt-3">
+  <div class="card-header bg-primary">
+    Sign In to BackOffice of Fitness Center
+  </div>
+  <div class="card-body">
+    <div>
+      <div>Username</div>
+      <input class="form-control" bind:value={username} />
+    </div>
+    <div class="mt-3">
+      <div>Password</div>
+      <input class="form-control" type="password" bind:value={password} />
+    </div>
+    <div class="mt-3">
+      <button class="btn btn-primary" on:click={() => signin()}>
+        <i class="fa fa-check me-2"></i>
+        Sign In Now
+      </button>
+    </div>
+  </div>
+</div>
